fa62ffda71f49341cba9484a7f1e327e73f2c73e
Chanan Damboritz <chanan.damboritz@klarna.com>
2013-05-04 16:48:22 +0300

    Finish sign-in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -51,6 +51,6 @@ describe "Static pages" do
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
     click_link "sample app"
-    page.should have_selector 'title', text: full_title('Sign up')
+    page.should have_selector 'title', text: full_title('')
   end
 end
